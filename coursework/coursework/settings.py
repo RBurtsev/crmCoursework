36414e485ce6053c55e32507fe833d109d606acf
RBurtsev <r.a.burtsev@mail.ru>
2022-09-22 06:26:51 +0300

    changed the main page to 'home' and also added the home/parts page

--- coursework/coursework/settings.py
+++ coursework/coursework/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'managedeck.apps.JiraConfig',
+    'home.apps.JiraConfig',
 ]
 
 MIDDLEWARE = [
--- coursework/home/views.py
+++ coursework/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-from .models import Tasks, Clients, Cars, Workers
+from django.views import generic
+from .models import Tasks, Clients, Cars, Workers, Parts, Fixes
 
 
 def index(request):
@@ -10,14 +10,19 @@ def index(request):
     # Генерация "количеств" некоторых главных объектов
     num_tasks = Tasks.objects.all().count()
     num_clients = Clients.objects.all().count()
-    # Доступные книги (статус = 'a')
     num_cars = Cars.objects.all().count()
     num_workers = Workers.objects.all().count()
-
+    num_fixes = Fixes.objects.all().count()
+    num_parts = Parts.objects.all().count()
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_tasks': num_tasks, 'num_clients': num_clients, 'num_cars': num_cars, 'num_workers': num_workers},
+        context={'num_tasks': num_tasks, 'num_clients': num_clients, 'num_cars': num_cars, 'num_workers': num_workers,
+                 'num_fixes': num_fixes, 'num_parts': num_parts},
     )
+
+class PartListView(generic.ListView):
+    model = Parts
+
