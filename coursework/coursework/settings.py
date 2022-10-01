31e9788e1333f721928392eb2a34022b5b705fe5
RBurtsev <r.a.burtsev@mail.ru>
2022-10-02 01:44:55 +0300

    change settings for host

--- coursework/coursework/settings.py
+++ coursework/coursework/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%l31_ozv%5xw)ya*45ocx^i4m3&harluezufu3#^di6m32cnp@'
+# SECRET_KEY = 'django-insecure-%l31_ozv%5xw)ya*45ocx^i4m3&harluezufu3#^di6m32cnp@'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
